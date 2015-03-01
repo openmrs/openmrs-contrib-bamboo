7bc121ed684eb4a0947f5a1f32195b1b1b0f2c7e
Cintia Del Rio Calvo <ccalvo@atlassian.com>
2015-03-01 22:29:01 +1100

    Adding error treatment; splitting release prepare and perform, trying to clean the rollback

--- release-prepare-perform.sh
+++ release-prepare-perform.sh
@@ -8,6 +8,10 @@ RELEASE_VERSION=""
 DEV_VERSION=""
 REMOTE_REPOSITORY=${bamboo_planRepository_repositoryUrl}
 
+RELEASE_PLUGIN="org.apache.maven.plugins:maven-release-plugin:2.5.1"
+
+echoerr() { echo "$@" 1>&2; }
+
 help(){
     echo -e "\n[HELP]"
     echo "Script to execute maven releases"
@@ -19,23 +23,25 @@ help(){
 }
 
 test_environment(){
-    # verify if the tag exists upstream v1.0.0 or *-1.0.0
-	if git ls-remote --tags origin | egrep -q "[-v]${RELEASE_VERSION}$"; then
-	    echo "[ERROR] Tag ${RELEASE_VERSION} already exists in the repo. Delete it before we can continue with the process."
-	    exit 1
-	fi 
 
-	if [[ "$RELEASE_VERSION" == "" ]]; then 
-		echo "RELEASE_VERSION = $RELEASE_VERSION"
-	    echo "[ERROR] At least one command line argument is missing. See the list above for reference. " 
+	if [[ "$RELEASE_VERSION" == "" || "$REMOTE_REPOSITORY" == "" ]]; then 
+		echoerr "RELEASE_VERSION = $RELEASE_VERSION\t REMOTE_REPOSITORY = $REMOTE_REPOSITORY"
+	    echoerr "[ERROR] At least one command line argument is missing. See the list above for reference. " 
 	    help 
 	    exit 1
 	fi 
 
-	if [[ "$$MAVEN_HOME" == "" ]]; then
-	    echo "[ERROR] MAVEN_HOME variable is unset. Make sure to set it using 'export MAVEN_HOME=/path/to/your/maven3/directory/'"
+	if [[ "$MAVEN_HOME" == "" ]]; then
+	    echoerr "[ERROR] MAVEN_HOME variable is unset. Make sure to set it using 'export MAVEN_HOME=/path/to/your/maven3/directory/'"
 	    exit 1
 	fi
+
+    # verify if the tag exists upstream v1.0.0 or .+-1.0.0
+    git remote set-url origin ${REMOTE_REPOSITORY}  # to make sure ls-remote works on Bamboo
+    if git ls-remote --tags origin | egrep -q "^(.+\-|v)${RELEASE_VERSION}$"; then
+        echoerr "[ERROR] Tag ${RELEASE_VERSION} already exists in the repo. Delete it before we can continue with the process."
+        exit 1
+    fi 
 }
 
 
@@ -47,13 +53,11 @@ while getopts "$ARGUMENTS_OPTS" opt; do
         d  ) DEV_VERSION=$OPTARG;;
         r  ) REMOTE_REPOSITORY=$OPTARG;;
         h  ) help; exit;;
-        \? ) echo "Unknown option: -$OPTARG" >&2; help; exit 1;;
-        :  ) echo "Missing option argument for -$OPTARG" >&2; help; exit 1;;
-        *  ) echo "Unimplemented option: -$OPTARG" >&2; help; exit 1;;
+        \? ) echoerr "Unknown option: -$OPTARG"; help; exit 1;;
+        :  ) echoerr "Missing option argument for -$OPTARG"; help; exit 1;;
+        *  ) echoerr "Unimplemented option: -$OPTARG"; help; exit 1;;
      esac
 done
-
-git remote set-url origin ${REMOTE_REPOSITORY}
 
 test_environment
 TEMP_FOLDER=$(mktemp -d -t release.XXXXXXX)
@@ -62,5 +66,19 @@ ARGS="-Dmaven.repo.local=$TEMP_FOLDER -DreleaseVersion=$RELEASE_VERSION"
 if [ "$DEV_VERSION" != "" ]; then
   ARGS+=" -DdevelopmentVersion=$DEV_VERSION"
 fi
- 
-$MAVEN_HOME/bin/mvn org.apache.maven.plugins:maven-release-plugin:2.5.1:prepare org.apache.maven.plugins:maven-release-plugin:2.5.1:perform ${ARGS} -B
+
+EXIT_CODE=0
+$MAVEN_HOME/bin/mvn ${RELEASE_PLUGIN}:prepare ${ARGS} -B || EXIT_CODE=$?
+if [[ "$EXIT_CODE" != "0" ]]; then
+    echoerr "[ERROR] mvn release:prepare failed. Attempting to do a release rollback. "
+    $MAVEN_HOME/bin/mvn ${RELEASE_PLUGIN}:rollback ${ARGS} -B || :
+    echoerr "[ERROR] mvn release:prepare failed, scroll up the logs to see the error. release:rollback was attempted. Delete any existent tag in the repo, fix the problem and try again. "
+    exit $EXIT_CODE
+fi
+
+EXIT_CODE=0
+$MAVEN_HOME/bin/mvn ${RELEASE_PLUGIN}:perform ${ARGS} -B || EXIT_CODE=$?
+if [[ "$EXIT_CODE" != "0" ]]; then
+    echoerr "[ERROR] mvn release:perform failed. Fix the problem and try another release number. "
+    exit $EXIT_CODE
+fi
