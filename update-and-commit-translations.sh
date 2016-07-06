ab6c77054ad89ceb391fbd771cabb089c328b9a0
Darius Jazayeri <jazayeri@alum.mit.edu>
2016-07-06 15:39:49 +0530

    Update update-and-commit-translations.sh

--- update-and-commit-translations.sh
+++ update-and-commit-translations.sh
@@ -3,7 +3,8 @@
 
 tx pull -f --mode=reviewed
 
-if [[ -z $(git diff-index --quiet HEAD --) ]]; then
-	git commit -am "committing translations from transifex"
-	git push
+if ! git diff-index --quiet HEAD --; then 
+  echo 'There are changes to be committed'
+  git commit -am "committing translations from transifex"
+  git push
 fi
