1aad5c8d3a6400ade338d1ee1b9fd6d34fe773fa
Cintia Del Rio Calvo <cintiadrio@gmail.com>
2016-09-20 21:41:54 +1000

    Setting remote URL on translations helper script

--- update-and-commit-translations.sh
+++ update-and-commit-translations.sh
@@ -5,7 +5,14 @@ tx pull -f --mode=reviewed
 
 git update-index -q --refresh
 
-if ! git diff-index --quiet HEAD --; then 
+# If running inside a Bamboo agent, set git origin
+if [ -n "${bamboo_shortJobName}" ]; then
+  remote_url="git@github.com:openmrs/openmrs-module-${bamboo_shortJobName}.git"
+  echo "Setting git remote to ${remote_url}"
+  git remote set-url origin $remote_url
+fi
+
+if ! git diff-index --quiet HEAD --; then
   echo 'There are changes to be committed'
   git commit -am "committing translations from transifex"
   git push
