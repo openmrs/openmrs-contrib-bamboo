432dd0900d9417e1addfcfdc12c03319e0d5aaa3
dkayiwa <dkayiwa@openmrs.org>
2018-11-28 22:29:01 +0300

    Testing if my alpha version change is allowed

--- release-prepare-perform.sh
+++ release-prepare-perform.sh
@@ -38,7 +38,7 @@ test_environment(){
 	    exit 1
 	fi
 
-  if [[ "$RELEASE_VERSION" != "" && ! "$RELEASE_VERSION" =~ ^([0-9]+\.){2}[0-9]+$ ]]; then
+  if [[ "$RELEASE_VERSION" != "" && ! "$RELEASE_VERSION" =~ ^([0-9]+\.){2}[0-9]+(-alpha)?$ ]]; then
     echoerr "[ERROR] Version $RELEASE_VERSION is not semver, e.g. 4.25.0. Check http://semver.org/ "
     exit 1
   fi
